4a9cedcba0918de08e30bbfe2dbbf904584422dd
Greg Harris <ghart.productions@gmail.com>
2021-05-07 17:11:29 -0700

    imported components into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import MainBody from "../components/MainBody";
+import Footer from "../components/Footer";
+import Wrapper from "../components/Wrapper";
 
 function App() {
   return (
     <div>
-      
+      <Wrapper>
+        <Header />
+        <Search />
+        <MainBody />
+        <Footer />
+      </Wrapper>
     </div>
   );
 }
