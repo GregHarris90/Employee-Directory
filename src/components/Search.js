a03dc18addeb79f09f23f9f50b297a1722570a98
Greg Harris <ghart.productions@gmail.com>
2021-05-08 00:22:25 -0700

    built out the search bar component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,21 @@
 import React from "react";
 
-function Search() {
-    return(
-        <div></div>
+function Search(props) {
+    return (
+        <div className = "searchbar">
+            <form>
+                <label htmlFor="search">Search:</label>
+            <input
+                // onChange={props / handleInputChange}
+                value={props.search}
+                name="search"
+                type="text"
+                placeholder="Search Employees"
+                id="search"
+            />
+            {/* <button onClick = {props.handleFormSubmit}> Search </button> */}
+            </form>
+        </div >
     );
 }
 
